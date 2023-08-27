7f5cf66397c287b65482dfc5776ce5a186c22a82
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-27 12:16:19 -0300

    inicia estilização de home

--- projeto de vendas online/src/views/Home/Home.vue
+++ projeto de vendas online/src/views/Home/Home.vue
@@ -1,4 +1,13 @@
 <template>
-       
-    
+      <h1>Produtos</h1> 
+      <v-card width="15%">
+        <v-img
+        src="https://m.media-amazon.com/images/I/81CnvOG8+YL._AC_SX522_.jpg"
+        class="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+        width="120px"
+        cover
+        />
+        
+      <v-card-title class="text-gray"></v-card-title> 
+      </v-card>
 </template>
