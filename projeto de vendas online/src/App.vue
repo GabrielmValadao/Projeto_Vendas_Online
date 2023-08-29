5325de8b9c5f2633bde3fa4a92a62a680e574368
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-28 21:59:59 -0300

    ultima estilização

--- projeto de vendas online/src/App.vue
+++ projeto de vendas online/src/App.vue
@@ -1,23 +1,30 @@
 <template>
-    <v-layout>
-      <v-app-bar color="rgb(169, 217, 208)"> 
-        <v-app-bar-title class="titulo">Funko Lab</v-app-bar-title>
-  
-        <v-btn icon>
-          <router-link to="/carrinho">
-          {{ this.$store.state.produtosCarrinho.length}}
-            <v-icon color="#FFF">mdi-cart</v-icon>
-          </router-link>
-        </v-btn>
-      </v-app-bar>
-  
-      <v-main>
-        <router-view></router-view>
-      </v-main>
-    </v-layout>
-  </template>
+    <div class="container">
 
-  <style scoped> 
+        <v-layout>
+            <v-app-bar color="rgb(169, 217, 208)"> 
+                <v-app-bar-title class="titulo">Funko Lab</v-app-bar-title>
+                
+                <v-btn icon>
+                    <router-link to="/carrinho">
+                        {{ this.$store.state.produtosCarrinho.length}}
+                        <v-icon color="#FFF">mdi-cart</v-icon>
+                    </router-link>
+                </v-btn>
+            </v-app-bar>
+            
+            <v-main>
+                <router-view></router-view>
+            </v-main>
+        </v-layout>
+    </div>
+    </template>
+
+  <style scoped>
+  
+  .container {
+    background-color: rgb(169, 217, 208);
+  }
 .titulo {
     display: flex;
     justify-content: center;
